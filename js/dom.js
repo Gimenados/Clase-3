aad7f69096bbb3855abefad331acfdd5422186b2
gimenados <gimenasande1@gmail.com>
2023-12-15 00:35:47 -0300

    15/12

--- js/dom.js
+++ js/dom.js
@@ -1,8 +1,8 @@
 //Document Object Model 
 
-//console.log(document.getElementById("Title")) //Me trae la etiqeuta vinculada de mi html
-//const title = document.getElementById("Title");
-//console.log(title.innerHTML)
+console.log(document.getElementById("Title").innerText) //Me trae la etiqeuta vinculada de mi html
+// const title = document.getElementById("Title");
+console.log(`${title.innerHTML}`)
 //title.innerHTML = "Hola mundo!"//Es propiedad el elemento title devuelve o establece la sintaxis HTML describiendo los descendientes del element
 //title.className = title.className + "Segunda clase"
 //title.style = "color: green"
@@ -38,50 +38,50 @@
 //Ejemplo del desafio 5 con funciones
 
 
-function modulo (a, b) { //Funcinoes con parametro 
-    return a % b;
-}
-function catConcat (stg, emoji) {
-    return stg.concat(emoji)
-}
+// function modulo (a, b) { //Funcinoes con parametro 
+//     return a % b;
+// }
+// function catConcat (stg, emoji) {
+//     return stg.concat(emoji)
+// }
 
-const cantGatos = window.prompt("Cuantos gatos quiere mostrar?");
+// const cantGatos = window.prompt("Cuantos gatos quiere mostrar?");
 
-function catList(cats) {
-    //parseInt te tranforma un "" a un number 
-    const catsTypeNumber = parseInt(cats, 10); //Quiero q me devuelva un numero
-    if (isNaN(catsTypeNumber)) {
-        return console.warn("ingresaste un valor incorrecto")
-    }
-    for (let i =1; i <= cats; i++) {
-        const mod = modulo(i, 3);
-        let catNumber = `Gato #${i}`;
-        if (mod === 1) {
-            catNumber = catConcat(catNumber, "😺");
-        } else if (mod === 2) {
-            catNumber = catConcat(catNumber, "😸");
-        } else {
-            catNumber = catConcat(catNumber, "😹");
-        }
-        console.log(catNumber)
-    }
-}
+// function catList(cats) {
+//     //parseInt te tranforma un "" a un number 
+//     const catsTypeNumber = parseInt(cats, 10); //Quiero q me devuelva un numero
+//     if (isNaN(catsTypeNumber)) {
+//         return console.warn("ingresaste un valor incorrecto")
+//     }
+//     for (let i =1; i <= cats; i++) {
+//         const mod = modulo(i, 3);
+//         let catNumber = `Gato #${i}`;
+//         if (mod === 1) {
+//             catNumber = catConcat(catNumber, "😺");
+//         } else if (mod === 2) {
+//             catNumber = catConcat(catNumber, "😸");
+//         } else {
+//             catNumber = catConcat(catNumber, "😹");
+//         }
+//         console.log(catNumber)
+//     }
+// }
 
-catList(cantGatos);
+// catList(cantGatos);
 
-//Otra forma de ver las funciones Arrows Functions  LLevan una flecha en su declaracion
+// //Otra forma de ver las funciones Arrows Functions  LLevan una flecha en su declaracion
 
-const f = x => {
-    return 2 * x + 1;
-}
+// const f = x => {
+//     return 2 * x + 1;
+// }
 
-//Otra manera de hacerlo
+// //Otra manera de hacerlo
 
-const f2 = x => 2 * x + 1;
+// const f2 = x => 2 * x + 1;
 
- const f3 = (x, y) => 2 * x * y;
+//  const f3 = (x, y) => 2 * x * y;
 
-//CHILDREN para acceder a los elementos hijos
+// //CHILDREN para acceder a los elementos hijos
 
 
 
